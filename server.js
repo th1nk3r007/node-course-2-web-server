c0eb471a20f1c1fc818a41242448e2df93e84865
th1nk3r007 <cerkovnik.ziga@gmail.com>
2018-06-20 16:06:41 -0500

    added portfolio view

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Portfolio Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Error'
